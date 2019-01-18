ee8d022a27e0579130691b297b254b932cefcc26
Lukas Domeisen <lukasdomeisen@Mac-2.local>
2019-01-18 11:43:46 +0100

    added backpack and map command

--- src/main/java/ch/bbw/zork/Backpack.java
+++ src/main/java/ch/bbw/zork/Backpack.java
@@ -3,11 +3,14 @@ package ch.bbw.zork;
 import java.util.ArrayList;
 
 public class Backpack { 
-	private ArrayList<Item> items;
+	private ArrayList<Item> items = new ArrayList<Item>();
 
 	
 	public void addItem(Item item) {
 		items.add(item);
+	}
+	public void removeItem(Item item) {
+		items.remove(item);
 	}
 	
 	public ArrayList<Item> getItems() {
@@ -16,5 +19,15 @@ public class Backpack {
 
 	public void setItems(ArrayList<Item> items) {
 		this.items = items;
+	}
+	
+	public Item getItem(String name) {
+		for (Item item : items) {
+			if (item.getName().equals(name)) {
+				items.remove(item);
+				return item;
+			}
+		}
+		return null;
 	}
 }
